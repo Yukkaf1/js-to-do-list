78aa76aef84648a7e22c417b6f3470deb7db29f9
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2022-11-09 14:20:01 +0100

    to-do-list

--- src/js/task-07.js
+++
@@ -1,7 +0,0 @@
-const imputEl = document.querySelector('input[id="font-size-control"]');
-const text = document.getElementById('text')
-imputEl.addEventListener('input', controlFontImput);
-
-function controlFontImput (event) {
-    text.style.fontSize = `${event.target.value}px`;
-}
