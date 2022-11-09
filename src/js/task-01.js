86055895861186af6d5ce183e0ca4e95da77e250
Trunova Yulia <103053349+Yukkaf1@users.noreply.github.com>
2022-11-09 14:54:20 +0100

    разметка

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,41 +1,17 @@
-// console.log("HW6-task-01");
+const getItemTemplate = (text) => 
+`<li class = "item">
+<div>
+  <input type="checkbox"/>
+  <span> ${text} </span>
+</div>
+<button type="button">x</button>
+</li>
+`
+const refs = {
+    list: document.querySelector('.list'),
+};
 
+const items = ['item 1', 'item 2', 'item 3'];
 
-
-const navEl = document.querySelectorAll("li.item");
-console.log(navEl);
-console.log("Number of categories:", navEl.length);
-console.log("");
-
-
-// const navEl0Text = navEl[0].querySelector("h2");
-// console.log("Category:", navEl0Text.textContent);
-
-// const El0 = navEl[0].querySelectorAll("li");
-// console.log("Elements:", El0.length);
-
-// console.log("");
-
-// const navEl1Text = navEl[1].querySelector("h2");
-// console.log("Category:", navEl1Text.textContent);
-
-// const El1 = navEl[1].querySelectorAll("li");
-// console.log("Elements:", El1.length);
-
-// console.log("");
-
-// const navEl2Text = navEl[2].querySelector("h2");
-// console.log("Category:", navEl2Text.textContent);
-
-// const El2 = navEl[2].querySelectorAll("li");
-// console.log("Elements:", El2.length);
-
-navEl.forEach((el) => {
-    const textEl = el.querySelector("h2");
-    console.log("Category:", textEl.textContent);
-
-    const ulEl = el.querySelectorAll("li");
-    console.log("Elements:", ulEl.length);
-    
-    console.log("");
-});
+const list = items.map((item) =>getItemTemplate(item));
+refs.list.insertAdjacentHTML('beforeend', list.join(''));
